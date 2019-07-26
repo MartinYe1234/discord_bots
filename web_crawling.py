1e525767d79826d934ffcef5673263b52c5e6934
unknown <martinye022@gmail.com>
2019-07-26 08:45:10 -0400

    changed web crawler

--- web_crawling.py
+++ web_crawling.py
@@ -1,6 +1,19 @@
+#import statements
 import requests
-import discord
 from bs4 import BeautifulSoup
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-page2 = BeautifulSoup(page.content,'html.parser')
-print(list(page2.children))
+
+#access page
+page = requests.get("https://en.wikipedia.org/wiki/Nuclear_power")
+soup = BeautifulSoup(page.content,'lxml')
+root1 = "https://en.wikipedia.org/"
+
+a = soup.find_all('a')
+links = [link.get('href') for link in a]
+print(links)
+for link in links:
+    root2 = link
+    print(root2)
+    next_page = root1 + root2
+    page2 = requests.get(next_page)
+    soup2 = BeautifulSoup(page2.content, 'lxml')
+    print(next_page)
