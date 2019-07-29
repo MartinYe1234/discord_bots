11c9db698ee0fff757dd221db6586011686e1925
unknown <martinye022@gmail.com>
2019-07-29 10:07:59 -0700

    basic bot functionality

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NjAwNjg2MDgyMTI3ODIyODQ4.XT5-NA.t4L4SnUmjEXaZVUDEDdr1lSkucI';
+const token = 'NjAwNjg2MDgyMTI3ODIyODQ4.XT8icA.BsxxG7N2VqOT4Lm8gzhytI9WDaw';
 
 const PREFIX = '!';
 
@@ -11,14 +11,34 @@ bot.on('ready', () =>{
 
 //listens for input from user
 bot.on('message', msg=>{
+
     //create array from anything after the PREFIX
     let args = msg.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
 
-        case 'ping':
-            msg.reply('pong');
-        break;
+        case 'info':
+
+            msg.channel.sendMessage('Type !search <tag> to generate a random picture with that tag');
+            break;
+
+        case 'search':
+
+            if (!args[1]){
+                msg.channel.send('You must specify a tag');
+            }
+            else{
+                var tag = args[1];
+            }
+            break;
+
+        case 'clear':
+
+            if(!args[1]){
+                return msg.reply('please specify how many to clear.');
+            }
+            var num_to_del = parseInt(args[1],10) + 1;
+            msg.channel.bulkDelete(num_to_del);
     }
 })
 
