187ab5d235753c58bc4fe40f90f5b98ed12a71fe
unknown <martinye022@gmail.com>
2019-07-28 22:18:57 -0700

    added switch statement

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NjAwNjg2MDgyMTI3ODIyODQ4.XT5QlA.Zn6uDAmPlUsqVNFNDVbK3CAOkLo';
+const token = 'NjAwNjg2MDgyMTI3ODIyODQ4.XT5-NA.t4L4SnUmjEXaZVUDEDdr1lSkucI';
+
+const PREFIX = '!';
 
 bot.on('ready', () =>{
     console.log('this bot is online');
 })
 
+//listens for input from user
 bot.on('message', msg=>{
-    if(msg.content === "HELLO"){
-        msg.reply("yes?");
+    //create array from anything after the PREFIX
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+
+        case 'ping':
+            msg.reply('pong');
+        break;
     }
 })
 
